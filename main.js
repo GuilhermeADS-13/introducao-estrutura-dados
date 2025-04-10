a01b4fff862aca4b8710c6ea0154d8197ce0da56
GuilhermeADS-13 <vianna.guilherme2020@uni9.edu.br>
2025-04-10 00:23:22 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,15 @@
+let alison_nota_av1 = 8;
+let alison_nota_av2 = 9.6;
+let alison_media = (alison_nota_av1 + alison_nota_av2) /2;
+
+console.log ("média alison:" + alison_media);
+
+console.log ("Exemplo com arrays")
+let notas_alison = [8, 9.6, ];
+console.log (notas_alison);
+
+console.log("AV1: "+ notas_alison[0]);
+console.log("AV2: "+ notas_alison[1]);
+notas_alison[2] = (notas_alison[0] + notas_alison[1]) /2;
+console.log ("Média: "+ notas_alison[2]);
+console.log ("Todas:"+ notas_alison);
